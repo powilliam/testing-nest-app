e9391d340c9a2711a39cdb073fb55d43fdc94f9a
William Porto <powilliam19@gmail.com>
2020-08-29 22:03:04 -0400

    chore: remove @nestjs/config and use Docker & docker-compose to setup development environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
 import { MeetingModule } from './meeting/meeting.module';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule, MeetingModule],
+  imports: [DatabaseModule, MeetingModule],
 })
 export class AppModule {}
